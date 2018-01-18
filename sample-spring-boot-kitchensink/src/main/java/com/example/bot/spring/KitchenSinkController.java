2db649980ba05512e8bd9eb07e35179b74323746
Mapomme78 <Jerome.Barois@gmail.com>
2018-01-18 22:50:09 +0100

    Cleanup

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -95,90 +95,42 @@ public class KitchenSinkController {
 
     @EventMapping
     public void handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
-        //handleSticker(event.getReplyToken(), event.getMessage());
-    }
+   }
 
     @EventMapping
     public void handleLocationMessageEvent(MessageEvent<LocationMessageContent> event) {
-        /*LocationMessageContent locationMessage = event.getMessage();
-        reply(event.getReplyToken(), new LocationMessage(
-                locationMessage.getTitle(),
-                locationMessage.getAddress(),
-                locationMessage.getLatitude(),
-                locationMessage.getLongitude()
-        ));*/
-    }
+   }
 
     @EventMapping
     public void handleImageMessageEvent(MessageEvent<ImageMessageContent> event) throws IOException {
-        // You need to install ImageMagick
-      /*  handleHeavyContent(
-                event.getReplyToken(),
-                event.getMessage().getId(),
-                responseBody -> {
-                    DownloadedContent jpg = saveContent("jpg", responseBody);
-                    DownloadedContent previewImg = createTempFile("jpg");
-                    system(
-                            "convert",
-                            "-resize", "240x",
-                            jpg.path.toString(),
-                            previewImg.path.toString());
-                    reply(((MessageEvent) event).getReplyToken(),
-                          new ImageMessage(jpg.getUri(), jpg.getUri()));
-                });*/
-    }
+   }
 
     @EventMapping
     public void handleAudioMessageEvent(MessageEvent<AudioMessageContent> event) throws IOException {
-       /* handleHeavyContent(
-                event.getReplyToken(),
-                event.getMessage().getId(),
-                responseBody -> {
-                    DownloadedContent mp4 = saveContent("mp4", responseBody);
-                    reply(event.getReplyToken(), new AudioMessage(mp4.getUri(), 100));
-                });*/
-    }
+   }
 
     @EventMapping
     public void handleVideoMessageEvent(MessageEvent<VideoMessageContent> event) throws IOException {
-        // You need to install ffmpeg and ImageMagick.
-        /*handleHeavyContent(
-                event.getReplyToken(),
-                event.getMessage().getId(),
-                responseBody -> {
-                    DownloadedContent mp4 = saveContent("mp4", responseBody);
-                    DownloadedContent previewImg = createTempFile("jpg");
-                    system("convert",
-                           mp4.path + "[0]",
-                           previewImg.path.toString());
-                    reply(((MessageEvent) event).getReplyToken(),
-                          new VideoMessage(mp4.getUri(), previewImg.uri));
-                });*/
-    }
+   }
 
     @EventMapping
     public void handleUnfollowEvent(UnfollowEvent event) {
-        log.info("unfollowed this bot: {}", event);
-    }
+   }
 
     @EventMapping
     public void handleFollowEvent(FollowEvent event) {
-        //String replyToken = event.getReplyToken();
-        //this.replyText(replyToken, "Got followed event");
-    }
+   }
 
     @EventMapping
     public void handleJoinEvent(JoinEvent event) {
-        //String replyToken = event.getReplyToken();
-        //this.replyText(replyToken, "Joined " + event.getSource());
-    }
+   }
 
     @EventMapping
     public void handlePostbackEvent(PostbackEvent event) {
         String replyToken = event.getReplyToken();
         switch (event.getPostbackContent().getData()) {
         case "lancement_mobilisation":
-        	String datetime = event.getPostbackContent().getParams().get("date de lancement");
+        	String datetime = event.getPostbackContent().getParams().get("datetime");
         	this.replyText(replyToken, "Déclaration de Guerre!!! \n"
 +"A tous les Francs Unis \n"
 +"\n"
@@ -202,9 +154,7 @@ public class KitchenSinkController {
 
     @EventMapping
     public void handleBeaconEvent(BeaconEvent event) {
-        //String replyToken = event.getReplyToken();
-        //this.replyText(replyToken, "Got beacon message " + event.getBeacon().getHwid());
-    }
+   }
 
     @EventMapping
     public void handleOtherEvent(Event event) {
@@ -236,25 +186,6 @@ public class KitchenSinkController {
         this.reply(replyToken, new TextMessage(message));
     }
 
-    private void handleHeavyContent(String replyToken, String messageId,
-                                    Consumer<MessageContentResponse> messageConsumer) {
-        final MessageContentResponse response;
-        try {
-            response = lineMessagingClient.getMessageContent(messageId)
-                                          .get();
-        } catch (InterruptedException | ExecutionException e) {
-            reply(replyToken, new TextMessage("Cannot get image: " + e.getMessage()));
-            throw new RuntimeException(e);
-        }
-        messageConsumer.accept(response);
-    }
-
-    private void handleSticker(String replyToken, StickerMessageContent content) {
-        reply(replyToken, new StickerMessage(
-                content.getPackageId(), content.getStickerId())
-        );
-    }
-    
 	static private SimpleDateFormat basicSDF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
 
     private void handleTextContent(String replyToken, Event event, TextMessageContent content)
@@ -289,30 +220,7 @@ public class KitchenSinkController {
                 }
                break;
             }
-//            case "bye": {
-//                Source source = event.getSource();
-//                if (source instanceof GroupSource) {
-//                    this.replyText(replyToken, "Leaving group");
-//                    lineMessagingClient.leaveGroup(((GroupSource) source).getGroupId()).get();
-//                } else if (source instanceof RoomSource) {
-//                    this.replyText(replyToken, "Leaving room");
-//                    lineMessagingClient.leaveRoom(((RoomSource) source).getRoomId()).get();
-//                } else {
-//                    this.replyText(replyToken, "Bot can't leave from 1:1 chat");
-//                }
-//                break;
-//            }
-//            case "confirm": {
-//                ConfirmTemplate confirmTemplate = new ConfirmTemplate(
-//                        "Do it?",
-//                        new MessageAction("Yes", "Yes!"),
-//                        new MessageAction("No", "No!")
-//                );
-//                TemplateMessage templateMessage = new TemplateMessage("Confirm alt text", confirmTemplate);
-//                this.reply(replyToken, templateMessage);
-//                break;
-//            }
-            case "mobilisation": {
+           case "mobilisation": {
                 String imageUrl = createUri("/static/buttons/1040.jpg");
                 ButtonsTemplate buttonsTemplate = new ButtonsTemplate(
                         imageUrl,
@@ -321,7 +229,7 @@ public class KitchenSinkController {
                         Arrays.asList(
                         		new DatetimePickerAction("Datetime",
                                         "lancement_mobilisation",
-                                        "date de lancement",
+                                        "datetime",
                                         basicSDF.format(new Date(System.currentTimeMillis())),
                                         "2100-12-31T23:59",
                                         "1900-01-01T00:00")
@@ -330,109 +238,7 @@ public class KitchenSinkController {
                 this.reply(replyToken, templateMessage);
                 break;
             }
-//            case "carousel": {
-//                String imageUrl = createUri("/static/buttons/1040.jpg");
-//                CarouselTemplate carouselTemplate = new CarouselTemplate(
-//                        Arrays.asList(
-//                                new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
-//                                        new URIAction("Go to line.me",
-//                                                      "https://line.me"),
-//                                        new URIAction("Go to line.me",
-//                                                "https://line.me"),
-//                                        new PostbackAction("Say hello1",
-//                                                           "hello こんにちは")
-//                                )),
-//                                new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
-//                                        new PostbackAction("言 hello2",
-//                                                           "hello こんにちは",
-//                                                           "hello こんにちは"),
-//                                        new PostbackAction("言 hello2",
-//                                                "hello こんにちは",
-//                                                "hello こんにちは"),
-//                                        new MessageAction("Say message",
-//                                                          "Rice=米")
-//                                )),
-//                                new CarouselColumn(imageUrl, "Datetime Picker", "Please select a date, time or datetime", Arrays.asList(
-//                                        new DatetimePickerAction("Datetime",
-//                                                "action=sel",
-//                                                "datetime",
-//                                                "2017-06-18T06:15",
-//                                                "2100-12-31T23:59",
-//                                                "1900-01-01T00:00"),
-//                                        new DatetimePickerAction("Date",
-//                                                "action=sel&only=date",
-//                                                "date",
-//                                                "2017-06-18",
-//                                                "2100-12-31",
-//                                                "1900-01-01"),
-//                                        new DatetimePickerAction("Time",
-//                                                "action=sel&only=time",
-//                                                "time",
-//                                                "06:15",
-//                                                "23:59",
-//                                                "00:00")
-//                                ))
-//                        ));
-//                TemplateMessage templateMessage = new TemplateMessage("Carousel alt text", carouselTemplate);
-//                this.reply(replyToken, templateMessage);
-//                break;
-//            }
-//            case "image_carousel": {
-//                String imageUrl = createUri("/static/buttons/1040.jpg");
-//                ImageCarouselTemplate imageCarouselTemplate = new ImageCarouselTemplate(
-//                        Arrays.asList(
-//                                new ImageCarouselColumn(imageUrl,
-//                                        new URIAction("Goto line.me",
-//                                                "https://line.me")
-//                                ),
-//                                new ImageCarouselColumn(imageUrl,
-//                                        new MessageAction("Say message",
-//                                                "Rice=米")
-//                                ),
-//                                new ImageCarouselColumn(imageUrl,
-//                                        new PostbackAction("言 hello2",
-//                                                "hello こんにちは",
-//                                                "hello こんにちは")
-//                                )
-//                        ));
-//                TemplateMessage templateMessage = new TemplateMessage("ImageCarousel alt text", imageCarouselTemplate);
-//                this.reply(replyToken, templateMessage);
-//                break;
-//            }
-//            case "imagemap":
-//                this.reply(replyToken, new ImagemapMessage(
-//                        createUri("/static/rich"),
-//                        "This is alt text",
-//                        new ImagemapBaseSize(1040, 1040),
-//                        Arrays.asList(
-//                                new URIImagemapAction(
-//                                        "https://store.line.me/family/manga/en",
-//                                        new ImagemapArea(
-//                                                0, 0, 520, 520
-//                                        )
-//                                ),
-//                                new URIImagemapAction(
-//                                        "https://store.line.me/family/music/en",
-//                                        new ImagemapArea(
-//                                                520, 0, 520, 520
-//                                        )
-//                                ),
-//                                new URIImagemapAction(
-//                                        "https://store.line.me/family/play/en",
-//                                        new ImagemapArea(
-//                                                0, 520, 520, 520
-//                                        )
-//                                ),
-//                                new MessageImagemapAction(
-//                                        "URANAI!",
-//                                        new ImagemapArea(
-//                                                520, 520, 520, 520
-//                                        )
-//                                )
-//                        )
-//                ));
-//                break;
-            default:
+           default:
                 log.info("Ignored unknown message {}: {}", replyToken, text);
                 break;
         }
@@ -456,33 +262,5 @@ public class KitchenSinkController {
             log.info("Interrupted", e);
             Thread.currentThread().interrupt();
         }
-    }
-
-    private static DownloadedContent saveContent(String ext, MessageContentResponse responseBody) {
-        log.info("Got content-type: {}", responseBody);
-
-        DownloadedContent tempFile = createTempFile(ext);
-        try (OutputStream outputStream = Files.newOutputStream(tempFile.path)) {
-            ByteStreams.copy(responseBody.getStream(), outputStream);
-            log.info("Saved {}: {}", ext, tempFile);
-            return tempFile;
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
-
-    private static DownloadedContent createTempFile(String ext) {
-        String fileName = LocalDateTime.now().toString() + '-' + UUID.randomUUID().toString() + '.' + ext;
-        Path tempFile = KitchenSinkApplication.downloadedContentDir.resolve(fileName);
-        tempFile.toFile().deleteOnExit();
-        return new DownloadedContent(
-                tempFile,
-                createUri("/downloaded/" + tempFile.getFileName()));
-    }
-
-    @Value
-    public static class DownloadedContent {
-        Path path;
-        String uri;
     }
 }
